d7fdf49670fbb7fa982511976714c35d6494f741
Zachary <zachary.cannon@hotmail.com>
2018-06-12 09:43:47 -0500

    Commit the stuff
    Added Git Ignore

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
+    var counter : Int = 1;
+    struct Pokemon {
+        let sprite: String
+    }
+    @IBOutlet weak var dexImage: UIImageView!
+    @IBOutlet weak var clickMe: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBAction func clickMePressed(_ sender: Any) {
+        clickMe.isEnabled = false
+            Alamofire.request("https://pokeapi.co/api/v2/pokemon/" + String(counter)).responseData {
+                response in
+                let json = JSON(response.result.value!)
+                let url = URL(string: json["sprites"]["front_default"].stringValue)
+                let data = NSData(contentsOf: url!)
+                let image = UIImage(data : data! as Data)
+                self.dexImage.image = image
+                self.nameLabel.text = json["name"].stringValue.capitalized
+                self.counter += 1
+                self.clickMe.isEnabled = true
+            }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Hello World' do
+pod 'Alamofire', '~> 4.7' 
+pod 'SwiftyJSON', '~> 4.0'
+# Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Hello World
+
+  target 'Hello WorldTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Hello WorldUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
